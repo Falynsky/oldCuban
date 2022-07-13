213e7ed4b6f579f888101c4c752a7930e8dbd333
Kamil Faliński <kamil.falinski@gmail.com>
2022-07-13 20:00:29 +0200

    Nowy ład + poprawki graficzne

--- scripts/script.js
+++ scripts/script.js
@@ -2,14 +2,18 @@ function toggleAccordion(x, elementsInAccordion) {
     let content = x.parentElement.parentElement.getElementsByClassName('content')[0];
     let arrow = x.getElementsByClassName('arrow')[0]
 
-    if (arrow.classList.length === 3) {
+    if (arrow.classList.length === 1) {
         x.style.background = '#EEEEEE';
+        arrow.style.padding = '0 0 .889rem 0';
+        arrow.style.transform = 'rotate(270deg)';
         content.style.maxHeight = elementsInAccordion * 3.5 + 'rem';
     } else {
         x.style.background = '#FFFFFF';
+        arrow.style.padding = '.889rem 0 0 0';
+        arrow.style.transform = 'rotate(90deg)';
         content.style.maxHeight = '0';
     }
 
-    arrow.classList.toggle('fa-angle-down');
+    arrow.classList.toggle('opened');
 
 }
