4b56c0f3430dfaa37d4bbb5bfe058146a8909f90
Kamil Faliński <kamil.falinski@gmail.com>
2022-09-11 12:40:12 +0200

    uzupełnienie napojów bezalkoholowych

--- scripts/generate-divs-rums.js
+++ scripts/generate-divs-rums.js
@@ -4,18 +4,20 @@ $(document).ready(function () {
         .then(data => {
             var tmp = '';
             var counter = 0;
-            let map = new Map(Object.entries(data));
+            let list = Object.entries(data);
 
-            $.each(map.get('Azja'), function (key, value) {
-                tmp += '                <div class="position-container">\n' +
-                    '                    <div class="first-row">\n' +
-                    '                        <div class="name">' + value.Nazwa + '</div>\n' +
-                    '                        <div class="price">' + value.Cena + ',-</div>\n' +
-                    '                    </div>\n' +
-                    '                    <div class="second-row">' + value.Składniki + '</div>\n' +
-                    '                </div>';
+            $.each(list, function (key, value) {
+                if(value[1].Region=='Pozostałe kraje i mieszanki rumów'){
+                    tmp += '                <div class="position-container">\n' +
+                        '                    <div class="first-row">\n' +
+                        '                        <div class="name">' + value[1].Nazwa + '</div>\n' +
+                        '                        <div class="price">' + value[1].Cena + '</div>\n' +
+                        '                    </div>\n' +
+                        '                    <div class="second-row">' + value[1].Rozmiar + '</div>\n' +
+                        '                </div>';
 
-                counter++;
+                    counter++;
+                }
             });
 
             console.log('counter: ' + counter + '\n\n')
@@ -25,3 +27,33 @@ $(document).ready(function () {
 
 
 });
+
+
+//
+// $(document).ready(function () {
+//     fetch('../scripts/data.json')
+//         .then(results => results.json())
+//         .then(data => {
+//             var tmp = '';
+//             var counter = 0;
+//             let map = new Map(Object.entries(data));
+//
+//             $.each(map.get('Anglia'), function (key, value) {
+//                 tmp += '                <div class="position-container">\n' +
+//                     '                    <div class="first-row">\n' +
+//                     '                        <div class="name">' + value.Nazwa + '</div>\n' +
+//                     '                        <div class="price">' + value.Cena + ',-</div>\n' +
+//                     '                    </div>\n' +
+//                     '                    <div class="second-row">' + value.Rozmiar + '</div>\n' +
+//                     '                </div>';
+//
+//                 counter++;
+//             });
+//
+//             console.log('counter: ' + counter + '\n\n')
+//             console.log(tmp)
+//             // $('#positionss').prepend(tmp);
+//         });
+//
+//
+// });
