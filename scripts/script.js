c7401793f14613998a3d6d6daa1a0bffe21c4325
Kamil Faliński <kamil.falinski@gmail.com>
2022-09-11 12:54:42 +0200

    uzupełnienie napojów bezalkoholowych

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,17 @@
-function toggleAccordion(x, elementsInAccordion) {
+function toggleAccordion(x, elementsInAccordion, deepness) {
     let content = x.parentElement.parentElement.getElementsByClassName('content')[0];
     let arrow = x.getElementsByClassName('arrow')[0]
-
+    let dotDotLevel = ''
+    while (deepness-- > 0) {
+        dotDotLevel += '../'
+    }
     if (arrow.classList.length === 1) {
         x.style.background = '#EEEEEE';
-        arrow.src = '../../../assets/hide.svg'
+        arrow.src = dotDotLevel + 'assets/hide.svg'
         content.style.maxHeight = elementsInAccordion * (4 + Math.sqrt(elementsInAccordion)) + 'rem';
     } else {
         x.style.background = '#FFFFFF';
-        arrow.src = '../../../assets/expand.svg'
+        arrow.src = dotDotLevel + 'assets/expand.svg'
         content.style.maxHeight = '0';
     }
 
